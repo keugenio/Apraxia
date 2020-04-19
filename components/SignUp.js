d335b641adc201428139865c050e1f6f205021d2
Kui <kui_eugenio@hotmail.com>
2020-04-19 06:52:47 -0700

    create Account working

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,49 +1,60 @@
 import React, { useState } from 'react';
-import { View, TouchableHighlight, Text } from 'react-native';
+import { ScrollView, View, TouchableHighlight, Text, Dimensions, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Input, Button, Icon, Card } from 'react-native-elements';
 import Colors from '../constants/Colors';
 import Firebase from '../components/Firebase';
 
+const screenHeight = Dimensions.get('window').height;
 export default function SignUp() {
   const [ firstName, setFirstName] = useState();
   const [ lastName, setLastName ] = useState();
   const [ email, setEmail ] = useState();
   const [ password, setPassword ] = useState();
+  const [ password2, setPassword2 ] = useState();
   const [error, setError] = useState();
   const navigation = useNavigation();
 
   function CreateAccount(){
-    Firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((result)=>{
-      result.user.updateProfile({
-        displayName: firstName
-      })
-    })
-    .then(()=>{
-      navigation.navigate('Main');
-    })
-    .catch((err)=>{
-      // if error on create account, 
-      setError(err);
-    })
+    if (firstName && lastName && email && password && password2 ){
+      if (password == password2){
+        Firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((result)=>{
+          result.user.updateProfile({
+            displayName: firstName
+          })
+        })
+        .then(()=>{
+          navigation.navigate('Main');
+        })
+        .catch((err)=>{
+          // if error on create account, 
+          setError(err);
+        })
+      } else {
+        setError('passwords do not match')
+      }
+    } else {
+      setError('please fill all fields')
+    }
   }
 
   return (
-    <View style={{backgroundColor:Colors.yellow, width:'95%', paddingLeft:10, paddingRight:10, borderRadius:25, borderColor:Colors.orange, borderWidth:3}}>
+    <ScrollView style={{width:'100%'}} contentContainerStyle={{backgroundColor:Colors.yellow, height:screenHeight+200}}>
+    
       <Text style={{alignSelf:'center', color:Colors.dark, margin:40, fontSize:20}}>Create New Account</Text>
       <View style={{backgroundColor:Colors.white, paddingTop:20, paddingBottom:20}}>
         <Input
           placeholder='first name'
           inputStyle = {{paddingLeft:10}}
-          onChangeText = {(firstName)=>setFirstName(firstName)}
+          onChangeText = {(firstName)=>{setFirstName(firstName); setError(null)}}
           value={firstName}
           label="First"
         />
         <Input
           placeholder='last name'
           inputStyle = {{paddingLeft:10}}
-          onChangeText = {(lastName)=>setLastName(lastName)}
+          onChangeText = {(lastName)=>{setLastName(lastName); setError(null)}}
           value={lastName}
           label="Last"
         />
@@ -52,7 +63,7 @@ export default function SignUp() {
           keyboardType='email-address'
           textContentType='emailAddress'
           inputStyle = {{paddingLeft:10}}
-          onChangeText = {(email)=>setEmail(email)}
+          onChangeText = {(email)=>{setEmail(email); setError(null)}}
           value = {email}
           label="Email"
         />
@@ -61,10 +72,18 @@ export default function SignUp() {
           secureTextEntry={true}
           textContentType='password'
           inputStyle = {{paddingLeft:10}}
-          onChangeText = {(password) => setPassword(password)}
+          onChangeText = {(password) => {setPassword(password); setError(null)}}
           value = {password}
           label="Password"
         />
+        <Input
+          secureTextEntry={true}
+          textContentType='password2'
+          inputStyle = {{paddingLeft:10}}
+          onChangeText = {(password) => {setPassword2(password); setError(null)}}
+          value = {password2}
+          label="Confirm passowrd"
+        />        
         <Text style={{color:'red', alignSelf:'center'}}>{error}</Text>
         <Button
           title="Create"
@@ -78,6 +97,7 @@ export default function SignUp() {
         style={{alignSelf:'flex-end', marginTop:20, marginRight:20}}>
         <Text style={{color:Colors.dark}}>Cancel</Text>
       </TouchableHighlight>
-    </View>
+   
+    </ScrollView>
   ) 
 };
